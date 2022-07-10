4ce2fabd1487996769193877bc40154ab68b9ac4
Romain Leroux <romain@leroux.dev>
2022-07-10 16:13:43 +0200

    Add cubes

--- src/shapes.rs
+++ src/shapes.rs
@@ -1,3 +1,5 @@
+use ordered_float::OrderedFloat;
+
 use crate::intersections::Intersection;
 use crate::materials::Material;
 use crate::matrices::Matrix;
@@ -8,14 +10,15 @@ use crate::tuples::{Point, Tuple, Vector};
 pub enum Shape {
     Sphere(Sphere),
     Plane(Plane),
+    Cube(Cube),
 }
 
 impl Shape {
     pub fn transform(&self) -> Matrix<4, 4> {
         match self {
-            &Self::Sphere(Sphere { transform, .. }) | &Self::Plane(Plane { transform, .. }) => {
-                transform
-            }
+            &Self::Sphere(Sphere { transform, .. })
+            | &Self::Plane(Plane { transform, .. })
+            | &Self::Cube(Cube { transform, .. }) => transform,
         }
     }
 
@@ -26,15 +29,18 @@ impl Shape {
             })
             | Self::Plane(Plane {
                 ref mut transform, ..
+            })
+            | Self::Cube(Cube {
+                ref mut transform, ..
             }) => transform,
         }
     }
 
     pub fn material(&self) -> Material {
         match self {
-            &Self::Sphere(Sphere { material, .. }) | &Self::Plane(Plane { material, .. }) => {
-                material
-            }
+            &Self::Sphere(Sphere { material, .. })
+            | &Self::Plane(Plane { material, .. })
+            | &Self::Cube(Cube { material, .. }) => material,
         }
     }
 
@@ -44,6 +50,9 @@ impl Shape {
                 ref mut material, ..
             })
             | Self::Plane(Plane {
+                ref mut material, ..
+            })
+            | Self::Cube(Cube {
                 ref mut material, ..
             }) => material,
         }
@@ -55,6 +64,7 @@ impl Shape {
         let local_normal = match self {
             Self::Sphere(s) => s.local_normal_at(local_point),
             Self::Plane(p) => p.local_normal_at(local_point),
+            Self::Cube(c) => c.local_normal_at(local_point),
         };
         let world_normal = Tuple::from(inverse.transpose() * local_normal);
         let world_normal = Vector::new(world_normal[0], world_normal[1], world_normal[2]);
@@ -73,6 +83,11 @@ impl Shape {
             Shape::Plane(p) => {
                 if let Some(xs) = p.local_intersect(local_ray) {
                     intersections.push(xs)
+                }
+            }
+            Shape::Cube(s) => {
+                if let Some(xs) = s.local_intersect(local_ray) {
+                    intersections.extend(xs)
                 }
             }
         }
@@ -100,6 +115,18 @@ impl From<Plane> for Shape {
 impl From<&Plane> for Shape {
     fn from(p: &Plane) -> Self {
         Self::Plane(*p)
+    }
+}
+
+impl From<Cube> for Shape {
+    fn from(c: Cube) -> Self {
+        Self::Cube(c)
+    }
+}
+
+impl From<&Cube> for Shape {
+    fn from(c: &Cube) -> Self {
+        Self::Cube(*c)
     }
 }
 
@@ -179,6 +206,94 @@ impl Plane {
 }
 
 impl Default for Plane {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// An axis-aligned bounding box (AABB) centered on the origin
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Cube {
+    pub transform: Matrix<4, 4>,
+    pub material: Material,
+}
+
+impl Cube {
+    pub const EPSILON: f32 = 1e-4;
+
+    pub fn new() -> Self {
+        Self {
+            transform: Matrix::identity(),
+            material: Material::default(),
+        }
+    }
+
+    fn check_axis(origin: f32, direction: f32) -> (f32, f32) {
+        let tmin_numerator = -1. - origin;
+        let tmax_numerator = 1. - origin;
+
+        let (tmin, tmax) = if direction.abs() >= Self::EPSILON {
+            (tmin_numerator / direction, tmax_numerator / direction)
+        } else {
+            (
+                tmin_numerator * f32::INFINITY,
+                tmax_numerator * f32::INFINITY,
+            )
+        };
+
+        if tmin > tmax {
+            (tmax, tmin)
+        } else {
+            (tmin, tmax)
+        }
+    }
+
+    pub fn local_intersect(&self, local_ray: Ray) -> Option<[Intersection; 2]> {
+        let (xtmin, xtmax) = Self::check_axis(local_ray.origin.x(), local_ray.direction.x());
+        let (ytmin, ytmax) = Self::check_axis(local_ray.origin.y(), local_ray.direction.y());
+        let (ztmin, ztmax) = Self::check_axis(local_ray.origin.z(), local_ray.direction.z());
+
+        let tmin = [xtmin, ytmin, ztmin]
+            .into_iter()
+            .map(OrderedFloat::from)
+            .max()
+            .unwrap();
+        let tmax = [xtmax, ytmax, ztmax]
+            .into_iter()
+            .map(OrderedFloat::from)
+            .min()
+            .unwrap();
+
+        if tmin > tmax {
+            None
+        } else {
+            Some([
+                Intersection::new(tmin.into(), self.into()),
+                Intersection::new(tmax.into(), self.into()),
+            ])
+        }
+    }
+
+    pub fn local_normal_at(&self, local_point: Point) -> Vector {
+        let maxc = [local_point.x(), local_point.y(), local_point.z()]
+            .into_iter()
+            .map(|v| OrderedFloat(v.abs()))
+            .max()
+            .unwrap();
+
+        if local_point.x().abs() == maxc.into() {
+            return Vector::new(local_point.x(), 0., 0.);
+        } else if local_point.y().abs() == maxc.into() {
+            return Vector::new(0., local_point.y(), 0.);
+        } else if local_point.z().abs() == maxc.into() {
+            return Vector::new(0., 0., local_point.z());
+        } else {
+            unreachable!()
+        }
+    }
+}
+
+impl Default for Cube {
     fn default() -> Self {
         Self::new()
     }
@@ -293,5 +408,55 @@ mod tests {
 
         let r = Ray::new(Point::new(0., -1., 0.), Vector::new(0., 1., 0.));
         assert!(p.local_intersect(r).unwrap() == Intersection::new(1., p.into()));
+    }
+
+    #[test]
+    fn cube_basics() {
+        let cases = vec![
+            (Point::new(5., 0.5, 0.), Vector::new(-1., 0., 0.), 4., 6.), // +x
+            (Point::new(-5., 0.5, 0.), Vector::new(1., 0., 0.), 4., 6.), // -x
+            (Point::new(0.5, 5., 0.), Vector::new(0., -1., 0.), 4., 6.), // +y
+            (Point::new(0.5, -5., 0.), Vector::new(0., 1., 0.), 4., 6.), // -y
+            (Point::new(0.5, 0., 5.), Vector::new(0., 0., -1.), 4., 6.), // +z
+            (Point::new(0.5, 0., -5.), Vector::new(0., 0., 1.), 4., 6.), // -z
+            (Point::new(0., 0.5, 0.), Vector::new(0., 0., 1.), -1., 1.), // inside
+        ];
+        for (origin, direction, t1, t2) in cases.into_iter() {
+            let c = Cube::new();
+            let r = Ray::new(origin, direction);
+            let xs = c.local_intersect(r).unwrap();
+            assert!(xs[0].t() == t1);
+            assert!(xs[1].t() == t2);
+        }
+
+        let cases = vec![
+            (Point::new(-2., 0., 0.), Vector::new(0.2673, 0.5345, 0.8018)),
+            (Point::new(0., -2., 0.), Vector::new(0.8018, 0.2673, 0.5345)),
+            (Point::new(0., 0., -2.), Vector::new(0.5345, 0.8018, 0.2673)),
+            (Point::new(2., 0., 2.), Vector::new(0., 0., -1.)),
+            (Point::new(0., 2., 2.), Vector::new(0., -1., 0.)),
+            (Point::new(2., 2., 0.), Vector::new(-1., 0., 0.)),
+        ];
+        for (origin, direction) in cases.into_iter() {
+            let c = Cube::new();
+            let r = Ray::new(origin, direction);
+            let xs = c.local_intersect(r);
+            assert!(xs.is_none());
+        }
+
+        let cases = vec![
+            (Point::new(1., 0.5, -0.8), Vector::new(1., 0., 0.)),
+            (Point::new(-1., -0.2, -0.9), Vector::new(-1., 0., 0.)),
+            (Point::new(-0.4, 1., -0.1), Vector::new(0., 1., 0.)),
+            (Point::new(0.3, -1., 0.7), Vector::new(0., -1., 0.)),
+            (Point::new(-0.6, 0.3, 1.), Vector::new(0., 0., 1.)),
+            (Point::new(0.4, 0.4, -1.), Vector::new(0., 0., -1.)),
+            (Point::new(1., 1., 1.), Vector::new(1., 0., 0.)),
+            (Point::new(-1., -1., -1.), Vector::new(-1., 0., 0.)),
+        ];
+        for (point, normal) in cases.into_iter() {
+            let c = Cube::new();
+            assert!(c.local_normal_at(point) == normal);
+        }
     }
 }
