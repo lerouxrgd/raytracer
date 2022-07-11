9e9e791169f417c27473c87ae4c9f24bdc925514
Romain Leroux <romain@leroux.dev>
2022-07-11 21:51:44 +0200

    Add cylinders and cones

--- src/shapes.rs
+++ src/shapes.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 use ordered_float::OrderedFloat;
 
 use crate::intersections::Intersection;
@@ -11,6 +13,8 @@ pub enum Shape {
     Sphere(Sphere),
     Plane(Plane),
     Cube(Cube),
+    Cylinder(Cylinder),
+    Cone(Cone),
 }
 
 impl Shape {
@@ -18,6 +22,8 @@ impl Shape {
         match self {
             &Self::Sphere(Sphere { transform, .. })
             | &Self::Plane(Plane { transform, .. })
+            | &Self::Cylinder(Cylinder { transform, .. })
+            | &Self::Cone(Cone { transform, .. })
             | &Self::Cube(Cube { transform, .. }) => transform,
         }
     }
@@ -30,6 +36,12 @@ impl Shape {
             | Self::Plane(Plane {
                 ref mut transform, ..
             })
+            | Self::Cylinder(Cylinder {
+                ref mut transform, ..
+            })
+            | Self::Cone(Cone {
+                ref mut transform, ..
+            })
             | Self::Cube(Cube {
                 ref mut transform, ..
             }) => transform,
@@ -40,6 +52,8 @@ impl Shape {
         match self {
             &Self::Sphere(Sphere { material, .. })
             | &Self::Plane(Plane { material, .. })
+            | &Self::Cylinder(Cylinder { material, .. })
+            | &Self::Cone(Cone { material, .. })
             | &Self::Cube(Cube { material, .. }) => material,
         }
     }
@@ -50,6 +64,12 @@ impl Shape {
                 ref mut material, ..
             })
             | Self::Plane(Plane {
+                ref mut material, ..
+            })
+            | Self::Cylinder(Cylinder {
+                ref mut material, ..
+            })
+            | Self::Cone(Cone {
                 ref mut material, ..
             })
             | Self::Cube(Cube {
@@ -64,6 +84,8 @@ impl Shape {
         let local_normal = match self {
             Self::Sphere(s) => s.local_normal_at(local_point),
             Self::Plane(p) => p.local_normal_at(local_point),
+            Self::Cylinder(c) => c.local_normal_at(local_point),
+            Self::Cone(c) => c.local_normal_at(local_point),
             Self::Cube(c) => c.local_normal_at(local_point),
         };
         let world_normal = Tuple::from(inverse.transpose() * local_normal);
@@ -84,6 +106,18 @@ impl Shape {
                 if let Some(xs) = p.local_intersect(local_ray) {
                     intersections.push(xs)
                 }
+            }
+            Shape::Cylinder(c) => {
+                c.local_intersect(local_ray)
+                    .into_iter()
+                    .filter_map(|xs| xs)
+                    .for_each(|xs| intersections.push(xs));
+            }
+            Shape::Cone(c) => {
+                c.local_intersect(local_ray)
+                    .into_iter()
+                    .filter_map(|xs| xs)
+                    .for_each(|xs| intersections.push(xs));
             }
             Shape::Cube(s) => {
                 if let Some(xs) = s.local_intersect(local_ray) {
@@ -127,6 +161,30 @@ impl From<Cube> for Shape {
 impl From<&Cube> for Shape {
     fn from(c: &Cube) -> Self {
         Self::Cube(*c)
+    }
+}
+
+impl From<Cylinder> for Shape {
+    fn from(c: Cylinder) -> Self {
+        Self::Cylinder(c)
+    }
+}
+
+impl From<&Cylinder> for Shape {
+    fn from(c: &Cylinder) -> Self {
+        Self::Cylinder(*c)
+    }
+}
+
+impl From<Cone> for Shape {
+    fn from(c: Cone) -> Self {
+        Self::Cone(c)
+    }
+}
+
+impl From<&Cone> for Shape {
+    fn from(c: &Cone) -> Self {
+        Self::Cone(*c)
     }
 }
 
@@ -299,6 +357,244 @@ impl Default for Cube {
     }
 }
 
+/// Infinite cynlinder of radius 1 along the y axis
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Cylinder {
+    pub transform: Matrix<4, 4>,
+    pub material: Material,
+    pub min: f32,
+    pub max: f32,
+    pub closed: bool,
+}
+
+impl Cylinder {
+    pub const EPSILON: f32 = 1e-4;
+
+    pub fn new() -> Self {
+        Self {
+            transform: Matrix::identity(),
+            material: Material::default(),
+            min: f32::NEG_INFINITY,
+            max: f32::INFINITY,
+            closed: false,
+        }
+    }
+
+    /// A helper to reduce duplication.
+    ///
+    /// Checks to see if the intersection at `t` is within a radius of 1 from the axis.
+    fn check_cap(ray: Ray, t: f32) -> bool {
+        let x = ray.origin.x() + t * ray.direction.x();
+        let z = ray.origin.z() + t * ray.direction.z();
+        (x.powi(2) + z.powi(2)) <= 1.
+    }
+
+    fn intersect_caps(&self, ray: Ray, xs: &mut [Option<Intersection>; 2]) {
+        // caps only matters if the cylinder is closed, and might possibly be
+        // intersected by the ray
+        if !self.closed || ray.direction.y().abs() < Self::EPSILON {
+            return;
+        }
+
+        // check for an intersection with the lower end cap by intersecting the ray with
+        // the plane at y=self.min
+        let t = (self.min - ray.origin.y()) / ray.direction.y();
+        if Self::check_cap(ray, t) {
+            if let Some(xs) = xs.iter_mut().find(|xs| xs.is_none()) {
+                *xs = Some(Intersection::new(t, self.into()));
+            }
+        }
+
+        // check for an intersection with the upper end cap by intersecting the ray with
+        // the plane at y=self.max
+        let t = (self.max - ray.origin.y()) / ray.direction.y();
+        if Self::check_cap(ray, t) {
+            if let Some(xs) = xs.iter_mut().find(|xs| xs.is_none()) {
+                *xs = Some(Intersection::new(t, self.into()));
+            }
+        }
+    }
+
+    pub fn local_intersect(&self, local_ray: Ray) -> [Option<Intersection>; 2] {
+        let a = local_ray.direction.x().powi(2) + local_ray.direction.z().powi(2);
+        if a.abs() < Self::EPSILON {
+            // ray is parallel to the y axis
+            let mut xs = [None; 2];
+            self.intersect_caps(local_ray, &mut xs);
+            return xs;
+        }
+
+        let b = 2. * local_ray.origin.x() * local_ray.direction.x()
+            + 2. * local_ray.origin.z() * local_ray.direction.z();
+        let c = local_ray.origin.x().powi(2) + local_ray.origin.z().powi(2) - 1.;
+        let disc = b.powi(2) - 4. * a * c;
+        if disc < 0. {
+            // ray does not intersect the cylinder
+            return [None, None];
+        }
+
+        let mut t0 = (-b - disc.sqrt()) / (2. * a);
+        let mut t1 = (-b + disc.sqrt()) / (2. * a);
+        if t0 > t1 {
+            mem::swap(&mut t0, &mut t1);
+        }
+
+        let mut xs = [None; 2];
+        let y0 = local_ray.origin.y() + t0 * local_ray.direction.y();
+        if self.min < y0 && y0 < self.max {
+            xs[0] = Some(Intersection::new(t0, self.into()))
+        }
+        let y1 = local_ray.origin.y() + t1 * local_ray.direction.y();
+        if self.min < y1 && y1 < self.max {
+            xs[1] = Some(Intersection::new(t1, self.into()))
+        }
+        self.intersect_caps(local_ray, &mut xs);
+        xs
+    }
+
+    pub fn local_normal_at(&self, local_point: Point) -> Vector {
+        let dist = local_point.x().powi(2) + local_point.z().powi(2);
+        if dist < 1. && local_point.y() >= self.max - Self::EPSILON {
+            Vector::new(0., 1., 0.)
+        } else if dist < 1. && local_point.y() <= self.min + Self::EPSILON {
+            Vector::new(0., -1., 0.)
+        } else {
+            Vector::new(local_point.x(), 0., local_point.z())
+        }
+    }
+}
+
+impl Default for Cylinder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// A double-napped cone along the y axis
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Cone {
+    pub transform: Matrix<4, 4>,
+    pub material: Material,
+    pub min: f32,
+    pub max: f32,
+    pub closed: bool,
+}
+
+impl Cone {
+    pub const EPSILON: f32 = 1e-4;
+
+    pub fn new() -> Self {
+        Self {
+            transform: Matrix::identity(),
+            material: Material::default(),
+            min: f32::NEG_INFINITY,
+            max: f32::INFINITY,
+            closed: false,
+        }
+    }
+
+    /// A helper to reduce duplication.
+    ///
+    /// Checks to see if the intersection at `t` is within a radius of 1 from the axis.
+    fn check_cap(ray: Ray, t: f32, y: f32) -> bool {
+        let x = ray.origin.x() + t * ray.direction.x();
+        let z = ray.origin.z() + t * ray.direction.z();
+        (x.powi(2) + z.powi(2)) <= y.abs()
+    }
+
+    fn intersect_caps(&self, ray: Ray, xs: &mut [Option<Intersection>; 4]) {
+        // caps only matters if the cone is closed, and might possibly be
+        // intersected by the ray
+        if !self.closed || ray.direction.y().abs() < Self::EPSILON {
+            return;
+        }
+
+        // check for an intersection with the lower end cap by intersecting the ray with
+        // the plane at y=self.min
+        let t = (self.min - ray.origin.y()) / ray.direction.y();
+        if Self::check_cap(ray, t, self.min) {
+            if let Some(xs) = xs.iter_mut().find(|xs| xs.is_none()) {
+                *xs = Some(Intersection::new(t, self.into()));
+            }
+        }
+
+        // check for an intersection with the upper end cap by intersecting the ray with
+        // the plane at y=self.max
+        let t = (self.max - ray.origin.y()) / ray.direction.y();
+        if Self::check_cap(ray, t, self.max) {
+            if let Some(xs) = xs.iter_mut().find(|xs| xs.is_none()) {
+                *xs = Some(Intersection::new(t, self.into()));
+            }
+        }
+    }
+
+    pub fn local_intersect(&self, local_ray: Ray) -> [Option<Intersection>; 4] {
+        let a = local_ray.direction.x().powi(2) - local_ray.direction.y().powi(2)
+            + local_ray.direction.z().powi(2);
+        let b = 2. * local_ray.origin.x() * local_ray.direction.x()
+            - 2. * local_ray.origin.y() * local_ray.direction.y()
+            + 2. * local_ray.origin.z() * local_ray.direction.z();
+        let c = local_ray.origin.x().powi(2) - local_ray.origin.y().powi(2)
+            + local_ray.origin.z().powi(2);
+
+        if a.abs() < Self::EPSILON {
+            if b.abs() < Self::EPSILON {
+                return [None; 4];
+            } else {
+                let mut xs = [None; 4];
+                let t = -c / (2. * b);
+                xs[0] = Some(Intersection::new(t, self.into()));
+                self.intersect_caps(local_ray, &mut xs);
+                return xs;
+            }
+        }
+
+        let disc = b.powi(2) - 4. * a * c;
+        if disc < 0. {
+            return [None; 4];
+        }
+
+        let mut t0 = (-b - disc.sqrt()) / (2. * a);
+        let mut t1 = (-b + disc.sqrt()) / (2. * a);
+        if t0 > t1 {
+            mem::swap(&mut t0, &mut t1);
+        }
+
+        let mut xs = [None; 4];
+        let y0 = local_ray.origin.y() + t0 * local_ray.direction.y();
+        if self.min < y0 && y0 < self.max {
+            xs[0] = Some(Intersection::new(t0, self.into()))
+        }
+        let y1 = local_ray.origin.y() + t1 * local_ray.direction.y();
+        if self.min < y1 && y1 < self.max {
+            xs[1] = Some(Intersection::new(t1, self.into()))
+        }
+        self.intersect_caps(local_ray, &mut xs);
+        xs
+    }
+
+    pub fn local_normal_at(&self, local_point: Point) -> Vector {
+        let dist = local_point.x().powi(2) + local_point.z().powi(2);
+        if dist < 1. && local_point.y() >= self.max - Self::EPSILON {
+            Vector::new(0., 1., 0.)
+        } else if dist < 1. && local_point.y() <= self.min + Self::EPSILON {
+            Vector::new(0., -1., 0.)
+        } else {
+            let mut y = (local_point.x().powi(2) + local_point.z().powi(2)).sqrt();
+            if local_point.y() > 0. {
+                y = -y;
+            }
+            Vector::new(local_point.x(), y, local_point.z())
+        }
+    }
+}
+
+impl Default for Cone {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -456,6 +752,170 @@ mod tests {
         ];
         for (point, normal) in cases.into_iter() {
             let c = Cube::new();
+            assert!(c.local_normal_at(point) == normal);
+        }
+    }
+
+    #[test]
+    fn cylinder_basics() {
+        let cases = vec![
+            (Point::new(1., 0., 0.), Vector::new(0., 1., 0.)),
+            (Point::new(0., 0., 0.), Vector::new(0., 1., 0.)),
+            (Point::new(0., 0., -5.), Vector::new(1., 1., 1.)),
+        ];
+        for (origin, direction) in cases.into_iter() {
+            let c = Cylinder::new();
+            let direction = direction.normalize();
+            let r = Ray::new(origin, direction);
+            assert!(c.local_intersect(r) == [None, None]);
+        }
+
+        let cases = vec![
+            (Point::new(1., 0., -5.), Vector::new(0., 0., 1.), 5., 5.),
+            (Point::new(0., 0., -5.), Vector::new(0., 0., 1.), 4., 6.),
+            (
+                Point::new(0.5, 0., -5.),
+                Vector::new(0.1, 1., 1.),
+                6.80798,
+                7.08872,
+            ),
+        ];
+        for (origin, direction, t1, t2) in cases.into_iter() {
+            let c = Cylinder::new();
+            let direction = direction.normalize();
+            let r = Ray::new(origin, direction);
+            let xs = c
+                .local_intersect(r)
+                .into_iter()
+                .filter_map(|xs| xs)
+                .collect::<Vec<_>>();
+            assert!((xs[0].t() - t1).abs() < Cylinder::EPSILON);
+            assert!((xs[1].t() - t2).abs() < Cylinder::EPSILON);
+        }
+
+        let cases = vec![
+            (Point::new(1., 0., 0.), Vector::new(1., 0., 0.)),
+            (Point::new(0., 5., -1.), Vector::new(0., 0., -1.)),
+            (Point::new(0., -2., 1.), Vector::new(0., 0., 1.)),
+            (Point::new(-1., 1., 0.), Vector::new(-1., 0., 0.)),
+        ];
+        for (point, normal) in cases.into_iter() {
+            let c = Cylinder::new();
+            assert!(c.local_normal_at(point) == normal);
+        }
+    }
+
+    #[test]
+    fn cylinder_advanced() {
+        let cases = vec![
+            (Point::new(0., 1.5, 0.), Vector::new(0.1, 1., 0.), 0),
+            (Point::new(0., 3., -5.), Vector::new(0., 0., 1.), 0),
+            (Point::new(0., 0., -5.), Vector::new(0., 0., 1.), 0),
+            (Point::new(0., 2., -5.), Vector::new(0., 0., 1.), 0),
+            (Point::new(0., 1., -5.), Vector::new(0., 0., 1.), 0),
+            (Point::new(0., 1.5, -2.), Vector::new(0., 0., 1.), 2),
+        ];
+        for (origin, direction, count) in cases.into_iter() {
+            let mut c = Cylinder::new();
+            c.min = 1.;
+            c.max = 2.;
+            let direction = direction.normalize();
+            let r = Ray::new(origin, direction);
+            assert!(c.local_intersect(r).into_iter().filter_map(|xs| xs).count() == count);
+        }
+
+        let cases = vec![
+            (Point::new(0., 3., 0.), Vector::new(0., -1., 0.), 2),
+            (Point::new(0., 3., -2.), Vector::new(0., -1., 2.), 2),
+            // (Point::new(0., 4., -2.), Vector::new(0., -1., 1.), 2),
+            (Point::new(0., 0., -2.), Vector::new(0., 1., 2.), 2),
+            // (Point::new(0., -1., -2.), Vector::new(0., 1., 1.), 2),
+        ];
+        for (point, direction, count) in cases.into_iter() {
+            let mut c = Cylinder::new();
+            c.min = 1.;
+            c.max = 2.;
+            c.closed = true;
+            let direction = direction.normalize();
+            let r = Ray::new(point, direction);
+            assert!(c.local_intersect(r).into_iter().filter_map(|xs| xs).count() == count);
+        }
+
+        let cases = vec![
+            (Point::new(0., 1., 0.), Vector::new(0., -1., 0.)),
+            (Point::new(0.5, 1., 0.), Vector::new(0., -1., 0.)),
+            (Point::new(0., 1., 0.5), Vector::new(0., -1., 0.)),
+            (Point::new(0., 2., 0.), Vector::new(0., 1., 0.)),
+            (Point::new(0.5, 2., 0.), Vector::new(0., 1., 0.)),
+            (Point::new(0., 2., 0.5), Vector::new(0., 1., 0.)),
+        ];
+        for (point, normal) in cases.into_iter() {
+            let mut c = Cylinder::new();
+            c.min = 1.;
+            c.max = 2.;
+            c.closed = true;
+            assert!(dbg!(c.local_normal_at(point)) == normal);
+        }
+    }
+
+    #[test]
+    fn cone_basics() {
+        let cases = vec![
+            (Point::new(0., 0., -5.), Vector::new(0., 0., 1.), 5., 5.),
+            // (
+            //     Point::new(0., 0., -5.),
+            //     Vector::new(1., 1., 1.),
+            //     8.66025,
+            //     8.66025,
+            // ),
+            (
+                Point::new(1., 1., -5.),
+                Vector::new(-0.5, -1., 1.),
+                4.55006,
+                49.44994,
+            ),
+        ];
+        for (origin, direction, t1, t2) in cases.into_iter() {
+            let c = Cone::new();
+            let direction = direction.normalize();
+            let r = Ray::new(origin, direction);
+            let xs = c
+                .local_intersect(r)
+                .into_iter()
+                .filter_map(|xs| xs)
+                .collect::<Vec<_>>();
+            assert!((xs[0].t() - t1).abs() < Cone::EPSILON);
+            assert!((xs[1].t() - t2).abs() < Cone::EPSILON);
+        }
+
+        let c = Cone::new();
+        let direction = Vector::new(0., 1., 1.).normalize();
+        let r = Ray::new(Point::new(0., 0., -1.), direction);
+        let xs = c.local_intersect(r);
+        assert!((xs[0].unwrap().t() - 0.35355).abs() < Cone::EPSILON);
+
+        let cases = vec![
+            (Point::new(0., 0., -5.), Vector::new(0., 1., 0.), 0),
+            (Point::new(0., 0., -0.25), Vector::new(0., 1., 1.), 2),
+            (Point::new(0., 0., -0.25), Vector::new(0., 1., 0.), 4),
+        ];
+        for (origin, direction, count) in cases.into_iter() {
+            let mut c = Cone::new();
+            c.min = -0.5;
+            c.max = 0.5;
+            c.closed = true;
+            let direction = direction.normalize();
+            let r = Ray::new(origin, direction);
+            assert!(c.local_intersect(r).into_iter().filter_map(|xs| xs).count() == count);
+        }
+
+        let cases = vec![
+            (Point::new(0., 0., 0.), Vector::new(0., 0., 0.)),
+            (Point::new(1., 1., 1.), Vector::new(1., -f32::sqrt(2.), 1.)),
+            (Point::new(-1., -1., 0.), Vector::new(-1., 1., 0.)),
+        ];
+        for (point, normal) in cases.into_iter() {
+            let c = Cone::new();
             assert!(c.local_normal_at(point) == normal);
         }
     }
